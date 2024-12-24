9b66c849722d39d0c779e049dc7a16e3d14a6e14
ylich <vyboluarte@minsait.com>
2024-12-24 18:10:43 +0100

    feat player memory

--- src/components/Tablero.tsx
+++ src/components/Tablero.tsx
@@ -6,10 +6,9 @@ import { Casilla, CasillaInterface } from '../interfaces/casilla';
 import { TableroInterface } from '../interfaces/Tablero';
 import { useState } from 'react';
 import { movimientoValido } from '../utils/flujo_validacion';
-
+import { COLOR_PIEZA } from '../constants';
 export default function TableroTag() {
     const [posicionTablero, setPosicionTablero] = useState<TableroInterface>(generateInitialState());
-    const [estaPresionada, setEstaPresionada]=useState(false);
     const [casillaOrigen, setCasillaOrigen]=useState<CasillaInterface>(
     new Casilla({
         color:'',
@@ -19,35 +18,66 @@ export default function TableroTag() {
         pieza:'',
         colorPieza:''
     }));
+    //player memory
+    const [estadoPartida, setEstadoPartida]=useState({
+        turno:0,
+        siguienteJugador:COLOR_PIEZA.BLANCO,
+        hayPiezaPresionada:false
+    })
 
+    function handlePosicionTablero(casillaDestino:CasillaInterface){
+        setPosicionTablero(posicionTablero.updateTableroAfterMovement(casillaOrigen,casillaDestino));
+    }
+    function handleEstadoPartida(){
+            setEstadoPartida((estadoPrevio)=>{
+                if(estadoPrevio.siguienteJugador===COLOR_PIEZA.BLANCO){
+                    return {...estadoPrevio,siguienteJugador:COLOR_PIEZA.NEGRO}
+                }else{
+                    return {...estadoPrevio,siguienteJugador:COLOR_PIEZA.BLANCO}
+                }
+            }
+        )
+    }
+    function handleEstaPresionada(estaPresionada:boolean){
+        setEstadoPartida((estadoPrevio)=>{
+            return({...estadoPrevio,hayPiezaPresionada:estaPresionada})
+        })
+    }
 
 
     function logicaMovimientoEsValido(casillaOrigen:CasillaInterface,posicionTablero:TableroInterface,casillaDestino:CasillaInterface){
         if (movimientoValido(casillaOrigen,posicionTablero,casillaDestino)){
-            setPosicionTablero(posicionTablero.updateTableroAfterMovement(casillaOrigen,casillaDestino));
+            handlePosicionTablero(casillaDestino)
+            handleEstadoPartida()
+            //guardar movimiento
+            //cambiar turno
             console.log('movimiento valido');
         }
     }
-    function logicaMemoriaPiezaTocada(casillaPresionada:CasillaInterface){
+    function handlePiezaTocada(casillaPresionada:CasillaInterface){
         setCasillaOrigen(casillaPresionada);
     }
 
     //hay que pasar toda la casilla
     function pulsacionEnTablero(casillaPresionada:CasillaInterface){
 
-        if (estaPresionada){
-            setEstaPresionada(false);
-            logicaMovimientoEsValido(casillaOrigen,posicionTablero,casillaPresionada);
-        }
-        else{
-            if (casillaPresionada.getPieza()){
-                setEstaPresionada(true);
-                logicaMemoriaPiezaTocada(casillaPresionada);}
+            if (estadoPartida.hayPiezaPresionada){
+
+                handleEstaPresionada(false)
+                logicaMovimientoEsValido(casillaOrigen,posicionTablero,casillaPresionada);
             }
+            else{
+                if (casillaPresionada.getPieza()){
+                    handleEstaPresionada(true)
+                    handlePiezaTocada(casillaPresionada);}
+                }
+        
+
     }
     const casillas=posicionTablero.getCasillas().map((casilla, index) => {
         
         return (
+            
             <CasillaTag 
             color={casilla.getColor()} 
             numeroCasilla={casilla.getNumero()}
@@ -60,10 +90,15 @@ export default function TableroTag() {
         )
     })
     return (
+        //meter esto en app?
+            <div>  
+                <div>
+                    {estadoPartida.siguienteJugador}
+                </div>
 
             <div className='tablero'>
                 {casillas}
             </div>
-        
+            </div>
     )
   }
